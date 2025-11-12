37ade0e6a63c0c3e0c52f9c7f244bc91dc84f075
Adam OBryan <obryan04@gmail.com>
2025-11-12 12:32:53 -0500

    updated some loading things

--- wp-content/themes/shepherd_marketing_site/header.php
+++ wp-content/themes/shepherd_marketing_site/header.php
@@ -5,34 +5,49 @@ $x_url = get_field('settings_footer_x_url', 'option');
 ?>
 <!DOCTYPE html>
 <html <?php language_attributes(); ?> class="no-js">
-	<head>
-		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	
-		<link rel="preconnect" href="https://fonts.googleapis.com">
-		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-		<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet">
 
-		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@12/swiper-bundle.min.css" />
-		<script src="https://cdn.jsdelivr.net/npm/swiper@12/swiper-bundle.min.js"></script>
-		<?php wp_head(); ?>
-	</head>
+<head>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@12/swiper-bundle.min.css" />
+    <script src="https://cdn.jsdelivr.net/npm/swiper@12/swiper-bundle.min.js"></script>
+
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-VW0Q2W115X"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-VW0Q2W115X');
+    </script>
+
+    <?php wp_head(); ?>
+</head>
 
 
-	<body>
-		<div id="page-wrapper">
-			<div class="page-gradient"></div>
-			<header class="clearfix">
-				<div class="inner-wrapper clearfix">
-					<div id="menu-toggle"></div>
-					<div id="header-logo">
-						<a href="<?php echo get_home_url(); ?>"><?php echo get_template_part('inc/header-logo'); ?></a>
-					</div>
-					<?php blankie_nav(); ?>
-					<div id="header-social">
-						<?php if ($facebook_url) { ?><a href="<?php echo $facebook_url; ?>" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/images/svg/icon-facebook-white.svg" /></a><?php } ?>
-						<?php if ($linkedin_url) { ?><a href="<?php echo $linkedin_url; ?>" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/images/svg/icon-linkedin-white.svg" /></a><?php } ?>
-						<?php if ($x_url) { ?><a href="<?php echo $x_url; ?>" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/images/svg/icon-x-white.svg" /></a><?php } ?>
-					</div>
-				</div>
-			</header>
+<body>
+    <div id="page-wrapper">
+        <div class="page-gradient"></div>
+        <header class="clearfix">
+            <div class="inner-wrapper clearfix">
+                <div id="menu-toggle"></div>
+                <div id="header-logo">
+                    <a href="<?php echo get_home_url(); ?>"><?php echo get_template_part('inc/header-logo'); ?></a>
+                </div>
+                <?php blankie_nav(); ?>
+                <div id="header-social">
+                    <?php if ($facebook_url) { ?><a href="<?php echo $facebook_url; ?>" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/images/svg/icon-facebook-white.svg" /></a><?php } ?>
+                    <?php if ($linkedin_url) { ?><a href="<?php echo $linkedin_url; ?>" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/images/svg/icon-linkedin-white.svg" /></a><?php } ?>
+                    <?php if ($x_url) { ?><a href="<?php echo $x_url; ?>" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/images/svg/icon-x-white.svg" /></a><?php } ?>
+                </div>
+            </div>
+        </header>
--- wp-content/themes/shepherd_marketing_site/library/main/enqueue-scripts.php
+++ wp-content/themes/shepherd_marketing_site/library/main/enqueue-scripts.php
@@ -6,21 +6,21 @@ function blankie_scripts()
       // Enqueue Jquery
       wp_enqueue_script( 'jquery', get_template_directory_uri() . '/js/jquery-3.5.0.js', array(), '3.5.0', false );
       // Enqueue Theme Scripts
-      wp_enqueue_script( 'scripts', get_template_directory_uri() . '/js/scripts.js', array( 'jquery' ), '1.0', true );
+      wp_enqueue_script( 'scripts', get_template_directory_uri() . '/js/scripts.js', array( 'jquery' ), filemtime(get_theme_file_path('/js/scripts.js')), true );
       // Enqueue slick header Scripts
-      wp_enqueue_script( 'header', get_template_directory_uri() . '/js/header.js', array( 'jquery' ), '1.0', true );
+      wp_enqueue_script( 'header', get_template_directory_uri() . '/js/header.js', array( 'jquery' ), filemtime(get_theme_file_path('/js/header.js')), true );
       // Plan Cards
-      wp_enqueue_script( 'plan-cards', get_template_directory_uri() . '/js/plan-cards.js', true );
+      wp_enqueue_script( 'plan-cards', get_template_directory_uri() . '/js/plan-cards.js',  filemtime(get_theme_file_path('/js/plan-cards.js')), true );
       // Heading Narrative
-      wp_enqueue_script( 'heading-narrative', get_template_directory_uri() . '/js/heading-narrative.js', true );
+      wp_enqueue_script( 'heading-narrative', get_template_directory_uri() . '/js/heading-narrative.js', filemtime(get_theme_file_path('/js/heading-narrative.js')), true );
       // Inc
-      wp_enqueue_script( 'inc', get_template_directory_uri() . '/js/inc.js', true );
+      wp_enqueue_script( 'inc', get_template_directory_uri() . '/js/inc.js', filemtime(get_theme_file_path('/js/inc.js')), true );
       // FAQ
-      wp_enqueue_script( 'faq', get_template_directory_uri() . '/js/faq.js', true );
+      wp_enqueue_script( 'faq', get_template_directory_uri() . '/js/faq.js', filemtime(get_theme_file_path('/js/faq.js')), true );
       // Card Tab Group
-      wp_enqueue_script( 'card-tab-group', get_template_directory_uri() . '/js/card-tab-group.js', true );
+      wp_enqueue_script( 'card-tab-group', get_template_directory_uri() . '/js/card-tab-group.js', filemtime(get_theme_file_path('/js/card-tab-group.js')), true );
       // Feature Tab Group
-      wp_enqueue_script( 'feature-tab-group', get_template_directory_uri() . '/js/feature-tab-group.js', true );
+      wp_enqueue_script( 'feature-tab-group', get_template_directory_uri() . '/js/feature-tab-group.js', filemtime(get_theme_file_path('/js/feature-tab-group.js')), true );
   }
 
 add_action('init', 'blankie_scripts'); // Add Scripts
